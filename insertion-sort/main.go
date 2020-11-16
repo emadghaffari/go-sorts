cdee764bfa2e4a1d767c0050d1e837dd47f658bb
Emad Ghaffari <emadghaffariii@gmail.com>
2020-11-16 16:09:40 +0000

    add insertion sort

---
+++ insertion-sort/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/emadghaffari/go-sorts/utils/random"
+)
+
+func main() {
+	items := random.Slice(20, 1, 15)
+	fmt.Println("Before: ", items)
+
+	result := insertion(items)
+
+	fmt.Println("After: ", result)
+}
+
+func insertion(items []int) []int {
+	n := len(items)
+	for i := 1; i < n; i++ {
+		j := i
+		for j > 0 {
+			if items[j-1] > items[j] {
+				items[j-1], items[j] = items[j], items[j-1]
+			}
+			j = j - 1
+		}
+	}
+	return items
+}
