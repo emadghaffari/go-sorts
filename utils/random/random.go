a460d26b2b909cb52313d3b11eb84203b2b1015c
Emad Ghaffari <emadghaffariii@gmail.com>
2020-11-16 16:11:19 +0000

    refactor random package

--- utils/random/random.go
+++ utils/random/random.go
@@ -1,12 +1,21 @@
 package random
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Slice random Slice of numbers
 func Slice(num, min, max int) []int {
-	items := make([]int, num)
+	if min > max {
+		fmt.Println("Error min and max")
+		return nil
+	}
+	rand.Seed(time.Now().UnixNano())
+	var items []int
 	for i := 0; i < num; i++ {
-		random := rand.Intn(max-min) + min
+		random := (rand.Intn((max - min)) + min)
 		items = append(items, random)
 	}
 	return items
