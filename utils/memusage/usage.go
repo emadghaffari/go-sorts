a5ff736905d5cd9aba7be39d2821689f1d5653c8
Emad Ghaffari <emadghaffariii@gmail.com>
2020-11-18 08:58:03 +0000

    add public func Print for check memory usage

---
+++ utils/memusage/usage.go
@@ -0,0 +1,22 @@
+package memusage
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Print outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func Print() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
