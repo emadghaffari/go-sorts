179a59ff2ec4e96b6b2307567dcc2b1f0f37595c
Emad Ghaffari <emadghaffariii@gmail.com>
2020-11-18 08:32:30 +0000

    add shell sort

---
+++ shell-sort/README.md
@@ -0,0 +1,11 @@
+# sell sort: (for very large lists)
+## idea from insertion sort
+## O(nlogn) === n scan list, logn passes
+## O(n power 3/2)
+## cap=[n/2], n = list length
+
+test result:
+BenchmarkInsertion-8   	   40254	     27115 ns/op	    2328 B/op	      20 allocs/op
+BenchmarkInsertion-8   	   42352	     27455 ns/op	    2328 B/op	      20 allocs/op
+BenchmarkInsertion-8   	   42352	     27455 ns/op	    2328 B/op	      20 allocs/op
+BenchmarkInsertion-8   	   48888	     26667 ns/op	    2328 B/op	      20 allocs/op
---
+++ shell-sort/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/emadghaffari/go-sorts/utils/random"
+)
+
+func main() {
+	items := random.Slice(20, 1, 15)
+	fmt.Println("Before: ", items)
+
+	result := shell(items)
+
+	fmt.Println("After : ", result)
+}
+
+func shell(items []int) []int {
+	var (
+		n    = len(items)
+		gaps = []int{1}
+		k    = 1
+	)
+	for {
+		gap := element(2, k) + 1
+		if gap > n-1 {
+			break
+		}
+		gaps = append([]int{gap}, gaps...)
+		k++
+	}
+
+	for _, gap := range gaps {
+		for i := gap; i < n; i += gap {
+			j := i
+			for j > 0 {
+				if items[j-gap] > items[j] {
+					items[j-gap], items[j] = items[j], items[j-gap]
+				}
+				j = j - gap
+			}
+		}
+	}
+	return items
+}
+
+func element(a, b int) int {
+	e := 1
+	for b > 0 {
+		if b&1 != 0 {
+			e *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return e
+}
